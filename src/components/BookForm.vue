ae4c5c917086f1a8167d80eb5158ab2b17f765e2
Nick Nack Attack <nickfarni001@gmail.com>
2021-01-27 13:14:55 -0700

    module 1 additions

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,14 +1,53 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
-    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
-    <label for="finishedReading">Finished Reading</label>
-    <input v-model="bookData.finishedReading" type="checkbox">
-    <label for="borrowed">borrowed</label>
-    <input v-model="bookData.ownership" type="radio" value="borrowed">
-    <label for="bought">bought</label>
-    <input v-model="bookData.ownership" type="radio" value="bought">
-    <button type="submit" name="button">Add Book</button>
+  <form
+    v-on:submit.prevent="bookSubmit(bookData)"
+    class=""
+    action="#"
+    method="post"
+  >
+    <input
+      v-model="bookData.bookTitle"
+      type="text"
+      name="title"
+      value=""
+      placeholder="Book Title"
+    >
+    <input
+      v-model="bookData.bookAuthor"
+      type="text"
+      name="author"
+      value=""
+      placeholder="Book Author"
+    >
+    <label for="finishedReading">
+      Finished Reading
+    </label>
+    <input
+      v-model="bookData.finishedReading"
+      type="checkbox"
+    >
+    <label for="borrowed">
+      borrowed
+    </label>
+    <input
+      v-model="bookData.ownership"
+      type="radio"
+      value="borrowed"
+    >
+    <label for="bought">
+      bought
+    </label>
+    <input
+      v-model="bookData.ownership"
+      type="radio"
+      value="bought"
+    >
+    <button
+      type="submit"
+      name="button"
+    >
+      Add Book
+    </button>
   </form>
 </template>
 
@@ -35,4 +74,7 @@ export default {
 </script>
 
 <style>
+form {
+  display: inline-grid;
+}
 </style>
